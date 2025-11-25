dc813821bbfa2488e6f2bab19411ac806e2e6dcd
Karol Linkiewicz <karol@linkiewicz.pl>
2025-11-25 18:06:02 +0100

    fix syntax

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,10 +5,11 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController {
+class MainController
+{
     #[Route('/')]
     public function home(): Response
     {
-        return new Response('<strong>Hi</strong> There!',$status = 200, $headers = ['Content-Type' => 'text/html']);
+        return new Response('<strong>Hi</strong> There!', $status = 200, $headers = ['Content-Type' => 'text/html']);
     }
 }
