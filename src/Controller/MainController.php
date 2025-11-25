0ccedc3f42561cfd9f5cf16b94697059deb6d519
Karol Linkiewicz <karol@linkiewicz.pl>
2025-11-25 18:49:32 +0100

    simple child template

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,22 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function home(): Response
     {
-        return new Response('<strong>Hi</strong> There!', $status = 200, $headers = ['Content-Type' => 'text/html']);
+        $starshipCount = 457;
+
+        $enterprise = ['name' => 'Enterprise', 'class' => 'Explorer', 'registry' => 'NCC-1701', 'captain'=> 'James T. Kirk'];
+        return $this->render('main/homepage.html.twig', [
+            'starship_count' => $starshipCount,
+            'myShip' => $enterprise,
+        ]);
+        //new Response('', $status = 200, $headers = ['Content-Type' => 'text/html']);
     }
 }
